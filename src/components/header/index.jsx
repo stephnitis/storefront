cf594bb705620d076df6be4773e4bdedce1e35bb
Stephanie Hill <stephni.hill@gmail.com>
2022-11-07 19:17:42 -0800

    header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,24 @@
+import { Toolbar } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Header = () => {
 
-  return(
+  return (
     <>
-    <h1>OUR STORE</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            OUR STORE
+          </Typography>
+          <Button color="inherit">CART</Button>
+          </Toolbar>
+
+        </AppBar>
+      </Box>
     </>
   )
 }
