231f244c65978c4da5e81baab721d22f2faf5196
Stephanie Hill <stephni.hill@gmail.com>
2022-11-09 10:31:08 -0800

    length

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,9 +3,12 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-// import { grey } from '@mui/material/colors';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  // employ the useSelector hook from react-redux
+  // takes a callback, that is state, and grab the state that you want
+  let productsInCart = useSelector(state => state.cart.productsInCart)
 
   return (
     <>
@@ -15,7 +18,7 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             OUR STORE
           </Typography>
-          <Button color="inherit">CART</Button>
+          <Button color="inherit">CART({productsInCart.length})</Button>
           </Toolbar>
         </AppBar>
       </Box>
